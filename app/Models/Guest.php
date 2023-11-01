6b476fe087c9c621ebae10560a81b7b5030667bd
Jaya-729 <programjaya729@gmail.com>
2023-11-01 11:59:34 +0700

    inisialisasi git

---
+++ app/Models/Guest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Guest extends Model
+{
+    use HasFactory;
+    protected $guarded = ['id']; // yang tidak boleh diisi oleh user
+    protected $nullable = ['tanggal_checkout']; // membuat suatu field boleh tidak di isi
+}
